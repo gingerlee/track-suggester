81ef58a9c426270c963394216e9ea05e6e91ec9c
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-10-20 10:44:56 -0700

    connect javascript and html so survey answer will show design card.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+  event.preventDefault();
+  var computer = $("#computer").val();
+  var work = $("#work").val();
+  var company = $("#company").val();
 
 
+  if (work === "ux" && computer === "mac" || work === "ux" && company === "agency" || computer === "mac" && comapny === "agency") {
+    $("#design").show();
+    $(".survey, #c, #ruby").hide();
+  } 
+
+
+  });
 });

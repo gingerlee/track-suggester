d39109a2717bdc01655bf718e7b0c1bfdf3ae124
Ginger Lee Kretschmer <kretschmons@gmail.com>
2017-10-20 15:34:14 -0700

    update javascript logic to make the survey results more accurate

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,11 @@ $(document).ready(function() {
     $("#design, .return").show();
     $(".survey, #c, #ruby").hide();
   } else if (work === "frontend" && company === "small" || work === "frontend" && company === "agency") {
-    $("#ruby, .return").show();
+    $("#ruby, .return, .matched").show();
     $(".survey, #design, #c").hide();
-  } else if (computer === "pc" || company === "enterprise" || work === "backend" || company === "enterprise" && work === "backend") {
-    $("#c, .return").show();
-
+  } else if (computer === "pc" && company === "enterprise" || computer === "pc" && work === "backend" || company === "enterprise" && work === "backend") {
+    $("#c, .return, .matched").show();
+    $(".survey, #design, #ruby").hide();
   } else {
     $(".nomatches, .return").show();
     $(".survey").hide();
@@ -25,13 +25,13 @@ $(document).ready(function() {
   if (firstName === "")  {
   $("input#first").addClass("is-invalid");
   $(".invalid-feedback#first, .survey").show();
-  $("#c, #design, #ruby").hide();
+  $("#c, #design, #ruby, .return").hide();
 }
 
 if (lastName === "")  {
 $("input#last").addClass("is-invalid");
 $(".invalid-feedback#last, .survey").show();
-$("#c, #design, #ruby").hide();
+$("#c, #design, #ruby, .return").hide();
 }
 
 
